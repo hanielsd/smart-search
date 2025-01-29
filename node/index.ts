9a66576089254953afeb869dc97f54e2b8670a53
Halefom <hannelalex12@gmail.com>
2025-01-29 08:41:43 +0300

    fetch data if there is unexpired cached data and store new data in cache

--- node/index.ts
+++ node/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import axios from 'axios'
+import { getData, storeData } from './cache'
 
 const app = express()
 
@@ -10,13 +11,19 @@ app.get('/', async (req, res) => {
   try {
     const query = (req.query.q as string) || ''
 
+    const cachedComments = getData(query)
+    if (cachedComments) {
+      res.status(200).send(cachedComments)
+      return
+    }
+
     const { data } = await axios.get(
       'https://jsonplaceholder.typicode.com/comments?postId=3',
     )
-
     const commentsFiltered = data.filter((comment: any) =>
       comment.name.toLowerCase().includes(query.toLowerCase()),
     )
+    storeData(query, commentsFiltered)
 
     res.status(200).send(commentsFiltered)
   } catch (e) {
