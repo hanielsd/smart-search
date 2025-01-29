5ddfa385c14fc570ff35e61892cee060d4add508
Halefom <hannelalex12@gmail.com>
2025-01-29 10:01:11 +0300

    refactored the request handler to extract the fetch and filter function so that it is easy for testing

--- node/index.ts
+++ node/index.ts
@@ -2,28 +2,33 @@ import express from 'express'
 import cors from 'cors'
 import axios from 'axios'
 import { getData, storeData } from './cache'
+import { JPHComment } from './types'
 
 const app = express()
 
 app.use(cors())
 
+export async function fetchNfilterComments(
+  query: string,
+): Promise<JPHComment[]> {
+  const cachedComments = getData(query)
+  if (cachedComments) return cachedComments
+
+  const { data } = await axios.get(
+    'https://jsonplaceholder.typicode.com/comments?postId=3',
+  )
+  const commentsFiltered = data.filter((comment: any) =>
+    comment.name.toLowerCase().includes(query.toLowerCase()),
+  )
+  storeData(query, commentsFiltered)
+
+  return commentsFiltered
+}
+
 app.get('/', async (req, res) => {
   try {
     const query = (req.query.q as string) || ''
-
-    const cachedComments = getData(query)
-    if (cachedComments) {
-      res.status(200).send(cachedComments)
-      return
-    }
-
-    const { data } = await axios.get(
-      'https://jsonplaceholder.typicode.com/comments?postId=3',
-    )
-    const commentsFiltered = data.filter((comment: any) =>
-      comment.name.toLowerCase().includes(query.toLowerCase()),
-    )
-    storeData(query, commentsFiltered)
+    const commentsFiltered = await fetchNfilterComments(query)
 
     res.status(200).send(commentsFiltered)
   } catch (e) {
