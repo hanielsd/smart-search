06bafe507fca0bf6bc88f17610a2c346b38f96d3
Halefom <hannelalex12@gmail.com>
2025-01-29 08:11:22 +0300

    fetching comments from the jsonplaceholder by post id and added a script to run the backend

--- node/index.ts
+++ node/index.ts
@@ -1,12 +1,21 @@
 import express from "express";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("/", async (req, res) => {
+  try {
+    // Fetch comments from the API
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/comments?postId=3"
+    );
+    console.log({ data });
+  } catch (e) {
+    res.status(500).send({ error: "Something went wrong, try again later!" });
+  }
 });
 
 app.listen(3001, () => {
