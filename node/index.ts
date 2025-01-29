c49b319bdc6ba8fd865b4c3fd984044967c96cb5
Halefom <hannelalex12@gmail.com>
2025-01-29 08:39:25 +0300

    formatted index.ts with prettierrc

--- node/index.ts
+++ node/index.ts
@@ -1,29 +1,29 @@
-import express from "express";
-import cors from "cors";
-import axios from "axios";
+import express from 'express'
+import cors from 'cors'
+import axios from 'axios'
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const query = (req.query.q as string) || "";
+    const query = (req.query.q as string) || ''
 
     const { data } = await axios.get(
-      "https://jsonplaceholder.typicode.com/comments?postId=3"
-    );
+      'https://jsonplaceholder.typicode.com/comments?postId=3',
+    )
 
     const commentsFiltered = data.filter((comment: any) =>
-      comment.name.toLowerCase().includes(query.toLowerCase())
-    );
+      comment.name.toLowerCase().includes(query.toLowerCase()),
+    )
 
-    res.status(200).send(commentsFiltered);
+    res.status(200).send(commentsFiltered)
   } catch (e) {
-    res.status(500).send({ error: "Something went wrong, try again later!" });
+    res.status(500).send({ error: 'Something went wrong, try again later!' })
   }
-});
+})
 
 app.listen(3001, () => {
-  console.log("Server is running on port 3001");
-});
+  console.log('Server is running on port 3001')
+})
